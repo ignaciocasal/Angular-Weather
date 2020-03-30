8d8c0fda561c8fc7b8f43627e34f0bb2262cbbae
Ignacio Casal <ignaciocasaldev@gmail.com>
2020-03-30 12:29:21 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   config: {
-    apiKey: "AIzaSyB0u5stLReraRoCy_OXYw8Oy_N0P3GRfmA",
-    authDomain: "angularweather-5c86c.firebaseapp.com",
-    databaseURL: "https://angularweather-5c86c.firebaseio.com",
-    projectId: "angularweather-5c86c",
-    storageBucket: "angularweather-5c86c.appspot.com",
-    messagingSenderId: "684747528560",
-    appId: "1:684747528560:web:17f27159597020401417b5"
+    apiKey: "XXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXX"
   },
-  appID: "3e8f6fb7c66ca51c962cc0f2dac7d6d5"
+  appID: "XXXXXXXXXXXXXXXX"
 };
