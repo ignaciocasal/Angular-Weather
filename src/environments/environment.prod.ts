d59bc8cb96424388ca64d1e3bc938966d5f24972
Ignacio <ignaciocasaldev@gmail.com>
2020-03-30 12:27:50 -0300

    Initialized firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: "AIzaSyB0u5stLReraRoCy_OXYw8Oy_N0P3GRfmA",
+    authDomain: "angularweather-5c86c.firebaseapp.com",
+    databaseURL: "https://angularweather-5c86c.firebaseio.com",
+    projectId: "angularweather-5c86c",
+    storageBucket: "angularweather-5c86c.appspot.com",
+    messagingSenderId: "684747528560",
+    appId: "1:684747528560:web:17f27159597020401417b5"
+  },
+  appID: "3e8f6fb7c66ca51c962cc0f2dac7d6d5"
 };
